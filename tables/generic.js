9a4a0293dd96ffa23322fb91cb2ca596f6c726ce
dshepsis <dfshepsis@gmail.com>
2022-05-30 20:21:28 -0400

    Using google sheets api
    I changed the code so it loads objectives from this Google Sheets
    spreadsheet:
    https://docs.google.com/spreadsheets/d/1eTmfECVVlp1nswOpbsviUjMK-cUwHziLK5nLn4rbuH4/edit?usp=sharing

--- tables/generic.js
+++ tables/generic.js
@@ -1,4 +1,62 @@
 import bingo from '../js/bingo.js'
+import {
+  GOOGLE_SHEETS_API_KEY,
+  GOOGLE_SHEETS_CLIENT_ID,
+} from '../authentication.js'
+
+// TODO(developer): Set to client ID and API key from the Developer Console
+const CLIENT_ID = GOOGLE_SHEETS_CLIENT_ID;
+const API_KEY = GOOGLE_SHEETS_API_KEY;
+
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
+
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
+
+/**
+ * Callback after api.js is loaded.
+ */
+window.onGoogleSheetsAPILoaded(() => {
+  gapi.load('client', parseObjectives);
+});
+
+async function parseObjectives() {
+  await gapi.client.init({
+    'apiKey':  API_KEY,
+    'discoveryDocs': [ DISCOVERY_DOC ],
+  });
+  let response;
+  try {
+    // Fetch first 10 files
+    response = await gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: '1eTmfECVVlp1nswOpbsviUjMK-cUwHziLK5nLn4rbuH4',
+      range: 'Objectives!A2:A',
+    });
+  } catch (err) {
+    throw err;
+  }
+  const range = response.result;
+  if (!range || !range.values || range.values.length == 0) {
+    throw err("Didn't get any data :(");
+  }
+  const challengePool = range.values.map(row => row[0]);
+  // Flatten to string to display
+  // const output = range.values.reduce(
+  //     (str, row) => `${str}${row[0]}, ${row[4]}\n`,
+  //     'Name, Major:\n');
+  // document.getElementById('content').innerText = output;
+  $(function() {
+    bingo(challengePool);
+    document.body.setAttribute('data-loading', 'false');
+  });
+}
+
 
 /**
  * A challenge that will be presented on the board.
@@ -23,113 +81,7 @@ import bingo from '../js/bingo.js'
  * @type {ChallengePool}
  */
  const challengePool = [
-  "Bloom 6 Cursed Patches",
-  "Complete 3 ball pushing minigames (Yami counts)",
-  // "Deliver the Acorn to Sleepy",
-  // "Deliver the Beehive to Sleepy",
-  "Deliver Inaba to the Animal Lover",
-  "Take Susano to Nagi’s Statue",
-  "Obtain 40 Demon Fangs",
-  "Use 6 Meat Feedbags",
-  "Use 6 Grass Feedbags",
-  "Use 6 Seeds Feedbags",
-  "Use 6 Fish Feedbags",
-  "Find 5 Zodiac Statues",
-  "Race Kai",
-  "Deliver the Prayer Slips to Rao",
-  "Upgrade 2 weapons With Gold Dust",
-  "Defeat 2 Blockheads",
-  "Rescue Chun",
-  "Get 3 Hidden Brush Techniques (Includes Upgrades)",
-  "Cut Down 2 Fruits From Sakuya’s Tree",
-  "Successfully enchant one of Yoichi’s arrows",
-  "Pick up all the treasures in Jamba’s Room",
-  "Clear 6 Devil Gates",
-  "Eat One of Mrs. Orange’s Cherry Cakes",
-  "Learn Hardhead",
-  "Get 10 Brush Techniques (Hidden/Upgrades Included)",
-  "Collect 10 Crystals",
-  "Deliver a Vase to 3 Guardian Statues",
-  "Use 3 Lockjaw Keys",
-  "Complete 2 Digging Minigames",
-  "Get Fed by 3 Separate NPCs",
-  "Get Whacked by 3 Separate NPCs",
-  "Sculpt 5 Snow Statues in Kamui",
-  "Race Tobi 3 Times",
-  "Fish in 3 different locations (Sei-an bridge and palace are separate)",
-  "Activate 8 Mermaid Springs",
-  "Successfully draw 2 popular designs for Mr. Chic",
-  "Turn 5 Oina Into Wolves",
-  "Collect 10 Pearls",
-  "Fight the Fire Doom Mirror",
-  "Get Attacked by 3 Different Animals",
-  "Defeat 3 Blue Demon Scrolls",
-  "Find 5 Incense Burners",
-  "Obtain 20 Stray Beads",
-  "Pick up all the treasures in the locked building in Sei-an",
-  "Obtain 2 Dungeon Maps",
-  "Do the Snowball Quest in Wep’Keer",
-  "Complete Komuso’s Challenge in Kusa",
-  "Learn 2 Dojo Weapon Techniques",
-  "Clear Mika’s Monster List (Shinshu)",
-  "Clear Haruka’s Monster List (Kusa)",
-  "Clear Wali’s Monster List (Kamui)",
-  "Clear the Samurai’s Monster List (Sei-an)",
-  "Get all Stray Beads from Kamiki Village",
-  "Obtain all Stray Beads from Sasa Sanctuary",
-  "Obtain all Stray Beads from Kusa Village",
-  "Obtain all Stray Beads from the Sunken Ship",
-  "Catch the Living Sword",
-  "Buy 2 Weapons",
-  "Obtain All Three End-Game Weapons",
-  "Finish the Ryoshima Devil Gate Trial",
-  "Die In-Game",
-  "Visit All the Islands in North Ryoshima",
-  "Take the Aspiring Carpenter to Naguri",
-  "Feed All the Cats in Catcall Tower",
-  "Obtain the Sun Fragment in Hana Valley",
-  "Collect the Snake Statue on the Twin Rocks in Ryoshima",
-  "Bloom the Clovers at the Top of Taka Pass",
-  "Get People Drunk in the Kamiki Festival",
-  "Interrupt Susano and Kushi’s Moment in Cave of Nagi",
-  "Push the Ball in Moon Cave",
-  "Fill In the Entire River of the Heavens",
-  "Fully Restore the Kusa Flower Patch",
-  "Fill an astral pouch without the aid of Rice Stew",
-  // "Eat 20 Different Types of Food", // Difficult to track
-  "Get Eaten by the Water Dragon",
-  "Deliver the Treasure Box to Urashima",
-  // "Swim in 15 Different Water Sources", // Difficult to track
-  "Talk to the Cutters in Cursed Taka",
-  "Obtain the Sun Fragment in Catcall Tower",
-  "Pay a Visit to Waka’s HQ",
-  "Catch 3 2-round or 3-round fish",
-  "Catch All the Night Fish From One Area",
-  "Catch All the Day Fish From One Area",
-  "Collect 15 Different Treasures",
-  "Buy From an Imp Merchant",
-  // "Buy From 10 Separate Merchants", // Difficult to track
-  // "Cross 4 Galestorm Bridges", // Difficult to track
-  "Lay a Reflector Smackdown on 3 Enemies in One Battle",
-  // "Get Repelled by 4 Different Barriers", // Difficult to track
-  "Bloom 3 Clovers Buried Under Hard Ground",
-  "Ride 3 Elevators",
-  "Deliver the Kusa Pinwheel to the Tea Customer",
-  "Obtain 5 Sun Fragments",
-  "Complete a Fish Page",
-  "Collect 10 Travel Guides",
-  "Obtain 6 Different Tassels",
-  "Complete a Bestiary Page",
-  "Defeat the Seaweed Monster in Sunken Ship",
-  "Fill In the Eyes of Two Daruma Dolls",
-  "Get the Holy Bone L From the Ryoshima Bell",
-  "Kill 3 Ghost Imps in Sunken Ship",
-  "Get 3 different fortunes from Madame Fawn",
-  "Talk to Isshaku",
-  "Steal an item with the Thief's Glove",
-  "Part the sea off Ryoshima Coast",
-  "Finish-off any enemy using Ink Bullet",
-  "Hold 3 items in Ammy's mouth (keys, sake, vases, etc.)"
-]
+
+];
 
 $(function() { bingo(challengePool); });
